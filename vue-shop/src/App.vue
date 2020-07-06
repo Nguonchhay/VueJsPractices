c108e6e713aadabe199f8edd619358428d50f551
Nguonchhay Touch <nguonchhay@web-essentials.co>
2020-07-06 13:50:56 +0700

    [TASK] Finish vue single page with shop

--- vue-shop/src/App.vue
+++ vue-shop/src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7">
+				<div class="row">
+					<div class="col-md-6" v-for="product in products" :key="product.id">
+						<product v-on:add-to-cart="addToCart(product)" :product="product" :isInCart="isInCart(product)"></product>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-5 my-5">
+				<cart v-on:pay="pay()" v-on:remove-from-cart="removeFromCart($event)" :items="cart"></cart>
+			</div>
+		</div>
+	</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import products from '@/products.json'
+import Product from '@/components/Product.vue'
+import Cart from '@/components/Cart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product,
+    Cart
+  },
+  data() {
+    return {
+      products,
+      cart: []
+    }
+  },
+  methods: {
+    addToCart(product) {
+      this.cart.push(product)
+    },
+
+    removeFromCart(product) {
+      this.cart = this.cart.filter(item => item.id !== product.id)
+    },
+
+    pay() {
+      this.cart = [];
+      alert('Pay successfully')
+    },
+
+    isInCart(product) {
+      const item = this.cart.find(p => p.id === product.id)
+      if (item) {
+        return true
+      }
+      return false
+    }
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+	body {
+		background-color: #FBFBFB;
+	}
 </style>
