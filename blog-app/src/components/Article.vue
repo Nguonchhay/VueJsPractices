d5c76ac0903e3add5be4e7868cf9cd12d5f8a20d
Nguonchhay Touch <nguonchhay@web-essentials.co>
2020-07-20 13:43:24 +0700

    [TASK] Blog: article list

---
+++ blog-app/src/components/Article.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="card">
+		<div class="card-header font-weight-bold">{{ article.title }}</div>
+		<div class="card-body">
+			<div class="my-3" v-html="article.content.substring(0, 200)"></div>
+			<button class="btn btn-primary">Read More</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['article']
+	}
+</script>
--- blog-app/src/views/Home.vue
+++ blog-app/src/views/Home.vue
@@ -1,5 +1,42 @@
 <template>
-	<h1 class="text-center my-5">
-		Welcome to Homepage
-	</h1>
+	<div>
+		<h1 class="text-center my-5">
+			Welcome to Homepage
+		</h1>
+
+		<div class="col-md-8 offset-md-2" v-for="article in articles" :key="article.id">
+			<Article :article="article"/>
+		</div>
+	</div>
 </template>
+
+<script>
+	import Axios from 'axios'
+	import config from '@/config'
+	import Article from '@/components/Article.vue'
+
+	export default {
+		data() {
+			return {
+				articles: {}
+			}
+		},
+		components: {
+			Article
+		},
+		mounted() {
+			this.getArticles()
+		},
+		methods: {
+			getArticles() {
+				Axios.get(`${config.apiUrl}/articles`)
+					.then(res => {
+						this.articles = res.data
+					})
+					.catch(err => {
+						console.log(err)
+					})
+			}
+		}
+	}
+</script>
