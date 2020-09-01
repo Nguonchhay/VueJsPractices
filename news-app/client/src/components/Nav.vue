145dcdb5578f5a2bd759b4a45a2d964135c3e14e
Nguonchhay Touch <nguonchhay@web-essentials.co>
2020-09-01 12:48:50 +0700

    [NEWSAPP] client with vuex and router

---
+++ news-app/client/src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/login">Login</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
